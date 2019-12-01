89e6435945cbdf70a0f05dcf69b0bf908ed5e1a2
Marian Lukac <marian.lukac18@imperial.ac.uk>
2019-12-01 03:31:23 +0100

    add include guards

--- SimpleHeaderAdd/extension.js
+++ SimpleHeaderAdd/extension.js
@@ -15,14 +15,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide   the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.addHeaders', AddHeader);
+	let addHeaders = vscode.commands.registerCommand('extension.addHeaders', AddHeader);
+	let IncludeGuards = vscode.commands.registerCommand('extension.addIncludeGuards', addIncludeGuardsCommand);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(addHeaders);
+	context.subscriptions.push(IncludeGuards);
+
 }
 exports.activate = activate;
 
@@ -34,18 +36,65 @@ function AddHeader() {
 	var cppPath = vscode.window.activeTextEditor.document.fileName;
 	var cppName = path.basename(cppPath);
 	if (/.*\.cpp/.test(cppName)) {
+		//checking for file hierarchy
 		if (/src\//.test(cppPath)) { hppPath = cppPath.replace(/src\/.*/, 'inc/' + cppName.replace(/\.cpp/, '.hpp')); }
 		else { hppPath = cppPath.replace(cppName, cppName.replace(/\.cpp/, '.hpp')); }
-		fs.appendFileSync(hppPath, '', 'utf8');
+		//creating the header
+		let created_new_file = false;
+		if (!fs.existsSync(hppPath)) {
+			fs.writeFileSync(hppPath, '', 'utf8');
+			created_new_file = true;
+		}
 		var openPath = vscode.Uri.file(hppPath); //A request file path
-
+		//switching to the file
 		vscode.workspace.openTextDocument(openPath).then(doc => {
-			vscode.window.showTextDocument(doc);
+			vscode.window.showTextDocument(doc).then(textEditor => {
+				let hppName = path.basename(hppPath).replace(/\..*/, '');
+				if (created_new_file) addIncludeGuards(hppName);
+			})
 		});
 	}
 	else {
 		vscode.window.showErrorMessage("Requires cpp file");
 	}
+}
+
+function addIncludeGuards(hppName) {
+	const textEditor = vscode.window.activeTextEditor;
+	//creating the guard
+	let headerGuard = "#ifndef " + hppName.toUpperCase() + "_HPP\n";
+	headerGuard += "#define " + hppName.toUpperCase() + "_HPP\n";
+	headerGuard += "\n#endif";
+	let position = textEditor.document.positionAt(30);
+	//inserting it into header
+	try {
+		textEditor.edit((edit) => {
+			edit.insert(position, headerGuard);
+		}).then((smh) => {
+			const editor = vscode.window.activeTextEditor;
+			const cur_position = editor.selection.active;
+
+			var newPosition = cur_position.with({ line: 2, character: 0 });
+			var newSelection = new vscode.Selection(newPosition, newPosition);
+			editor.selection = newSelection;
+		});
+	}
+	catch (error) {
+		if (error instanceof Error) {
+			vscode.window.showErrorMessage(error.message);
+		} else {
+			vscode.window.showErrorMessage('Unknown exception occured');
+		}
+
+	}
+	//moving cursor
+
+}
+
+function addIncludeGuardsCommand() {
+	let hppPath = vscode.window.activeTextEditor.document.fileName;
+	let hppName = path.basename(hppPath).replace(/\..*/, '');
+	addIncludeGuards(hppName);
 }
 
 module.exports = {
